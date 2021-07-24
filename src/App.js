411005b2a906b158e25c031cae55291621cc6e81
Dan33311 <60445863+Dan33311@users.noreply.github.com>
2021-07-24 14:29:21 -0500

    add our first  reusable component

--- src/App.js
+++ src/App.js
@@ -3,9 +3,6 @@ import Home from './Home'
 
 
 function App() {
-  const title = 'My App component'
-  const person = { name: "Dan", age: 36}
-  const link = "www.google.com"
   return (
     <div className="App">
       <Navbar />
--- src/Home.js
+++ src/Home.js
@@ -1,9 +1,18 @@
+import { useState } from 'react'
+import BlogList from './Blog-list'
+
 const Home = () => {
+  const [blogs, setBlogs] = useState([
+    { title: 'web development fundamentals', body: 'lorem ipsum...', teacher: 'William', id: 1},
+    { title: 'fullstack', body: 'lorem ipsum...', teacher: 'Jose', id: 2},
+    { title: 'Nodejs tutorial', body: 'lorem ipsum...', teacher: 'Shaun', id: 3}
+  ])
+  
   return (
     <div className="home">
-      <h2>Homepage TEST</h2>
+      <BlogList blogs={blogs} title="All Blogs"/>
     </div>
   )
 }
- 
+
 export default Home;
