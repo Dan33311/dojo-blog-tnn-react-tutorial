21a2fc7d363ef9b64e559511e4ac3133bdde0b0d
Dan33311 <60445863+Dan33311@users.noreply.github.com>
2021-07-26 22:31:36 -0500

    fetch data with JSON server and create our first custom Hook

--- src/Blog-list.js
+++ src/Blog-list.js
@@ -1,15 +1,11 @@
-
-
 const BlogList = ({ blogs, title, handleDelete }) => {
   
   return (
     <div className="blog-list">
-      <h2>{ title }</h2>
       {blogs.map(blog => (
         <div className="blog-preview" key={blog.id}>
           <h2>{blog.title}</h2>
-          <p>Instructor: {blog.teacher}</p>
-          <button onClick={() => handleDelete(blog.id)}>Delete</button>
+          <p>Written by: {blog.author}</p>
         </div>
       ))}
     </div>
--- src/Home.js
+++ src/Home.js
@@ -1,30 +1,18 @@
 import { useState, useEffect } from 'react'
 import BlogList from './Blog-list'
+import useFetch from './useFetch'
 
 const Home = () => {
-  const [blogs, setBlogs] = useState([
-    { title: 'web development fundamentals', body: 'lorem ipsum...', teacher: 'William', id: 1},
-    { title: 'fullstack', body: 'lorem ipsum...', teacher: 'Jose', id: 2},
-    { title: 'Nodejs tutorial', body: 'lorem ipsum...', teacher: 'Shaun', id: 3}
-  ])
-  
-  const handleDelete = (id) => {
-    const newBlogs = blogs.filter(blog => blog.id !== id)
-    setBlogs(newBlogs)
-  }
-
-  useEffect(() => {
-    console.log('use effect ran')
-    console.log(blogs)
-  })
+  // I have to run the port 8000 localy
+  const { error, isPending, data: blogs } = useFetch('http://localhost:8000/blogs')
 
   return (
     <div className="home">
-      <BlogList blogs={blogs} title="All Blogs" handleDelete={handleDelete}/>
+      { error && <div>{error}</div> }
+      { isPending && <div>Loading...</div> }
+      { blogs && <BlogList blogs={blogs} /> }
     </div>
   )
 }
 
 export default Home;
-
-// add a button that delete a blog
